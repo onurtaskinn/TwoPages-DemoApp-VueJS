00ad9621d38c77df50a5580c4b8df33f8faffcc9
Sammi Chung <20231784@life.hkbu.edu.hk>
2023-03-16 23:09:40 +0100

    First commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup>
+    import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <div>
+     <h1>Example of Router usage</h1>
+     <router-view>
+     </router-view>
+     <a class="button" ><router-link to="/page-one">Click for Page 1</router-link></a>&nbsp;
+        <a class="button" ><router-link to="/page-two">Click for Page 2</router-link></a>
+  </div>
+</template>
+
+<script>
+  export default {}
+</script>
---
+++ src/board.vue
@@ -0,0 +1,2 @@
+<a class="button" ><router-link to="/page-one">Click for Page 1</router-link></a>&nbsp;
+<a class="button" ><router-link to="/page-two">Click for Page 2</router-link></a>
